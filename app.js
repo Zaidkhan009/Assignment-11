7fda6581a881fc1c8b6efddbdd7434ccc2139d17
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-19 01:44:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+// EXERCISE 11
+//  let members: string[] =['Zaid', 'Azeem', 'Huzaifa' , 'Atif ' , 'Umer'];
+//  for(let i=0; i<members.length; i++){
+//      console.log(members[i]);
+//  }
+// EXERCISE 12
+let members = ['Zaid', 'Azeem', 'Huzaifa', 'Atif ', 'Umer'];
+let message = 'kal birthday hai kis ki:';
+for (let i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,11 @@
+// EXERCISE 11
+//  let members: string[] =['Zaid', 'Azeem', 'Huzaifa' , 'Atif ' , 'Umer'];
+//  for(let i=0; i<members.length; i++){
+//      console.log(members[i]);
+//  }
+// EXERCISE 12
+let members: string[] =['Zaid', 'Azeem', 'Huzaifa' , 'Atif ' , 'Umer'];
+let message: string='kal birthday hai kis ki:';
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
